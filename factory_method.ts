90598e89abfbbaf7c418f0b69d02430c687af745
JeongSeongHoon <jsh@seonghoon-mac.local>
2022-06-06 16:22:10 +0900

    [feat] singleton, factory_method

---
+++ factory_method.ts
@@ -0,0 +1,37 @@
+interface Ship {
+  operation(): void;
+}
+
+class WhiteShip implements Ship {
+  operation(): void {
+    console.log("WhiteShip");
+  }
+}
+
+class BlackShip implements Ship {
+  operation(): void {
+    console.log("BlackShip");
+  }
+}
+
+interface ShipFactory {
+  createShip(): Ship;
+}
+
+class WhiteShipFactory implements ShipFactory {
+  createShip(): WhiteShip {
+    return new WhiteShip();
+  }
+}
+
+class BlackShipFactory implements ShipFactory {
+  createShip(): BlackShip {
+    return new BlackShip();
+  }
+}
+
+const whiteShip = new WhiteShipFactory().createShip();
+const blackShip = new BlackShipFactory().createShip();
+
+whiteShip.operation();
+blackShip.operation();
